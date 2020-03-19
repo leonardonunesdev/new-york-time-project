49378b625ceacf593e117838e1ed17b327d4e992
Leonardo Nunes dos Santos <leonardonunes.255@hotmail.com>
2020-03-19 10:08:58 -0300

    Added comments

--- newYorkTimeProject/src/services/news.service.ts
+++ newYorkTimeProject/src/services/news.service.ts
@@ -9,6 +9,7 @@ export class NewsService {
 
   constructor(private http: HttpClient) { }
 
+  //Get informations from API, based on the selected page
   public getNewsNewYorkTime(page): Observable<any>{
     var urlApi = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:(%22Science%22%20%22Technology%22)&page=" + page + "&api-key=JEv05bTrtl8dtya0Sh1cCaJ1b141NGIo";
 
